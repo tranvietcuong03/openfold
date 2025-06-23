e4704af4d8d3ca4c5be8306ef435bfe5bc0a0e90
HuyCuong <caohuycuong85@gmail.com>
2025-06-24 06:22:22 +0700

    clean config and feature_pipeline hc-v1

--- vinafold/data/feature_pipeline.py
+++ vinafold/data/feature_pipeline.py
@@ -31,17 +31,7 @@ def np_to_tensor_dict(
     np_example: Mapping[str, np.ndarray],
     features: Sequence[str],
 ) -> TensorDict:
-    """Creates dict of tensors from a dict of NumPy arrays.
-
-    Args:
-        np_example: A dict of NumPy feature arrays.
-        features: A list of strings of feature names to be returned in the dataset.
-
-    Returns:
-        A dictionary of features mapping feature names to features. Only the given
-        features are returned, all other ones are filtered out.
-    """
-    # torch generates warnings if feature is already a torch Tensor
+    """Creates dict of tensors from a dict of NumPy arrays."""
     to_tensor = lambda t: torch.tensor(t) if type(t) != torch.Tensor else t.clone().detach()
     tensor_dict = {
         k: to_tensor(v) for k, v in np_example.items() if k in features
@@ -49,44 +39,31 @@ def np_to_tensor_dict(
 
     return tensor_dict
 
-
 def make_data_config(
     config: ml_collections.ConfigDict,
-    mode: str,
     num_res: int,
 ) -> Tuple[ml_collections.ConfigDict, List[str]]:
     cfg = copy.deepcopy(config)
-    mode_cfg = cfg[mode]
+    mode_cfg = cfg["train"]
     with cfg.unlocked():
         if mode_cfg.crop_size is None:
             mode_cfg.crop_size = num_res
 
     feature_names = cfg.common.unsupervised_features
-
-    # Add seqemb related features if using seqemb mode.
-    if cfg.seqemb_mode.enabled:
-        feature_names += cfg.common.seqemb_features
-
-    if cfg.common.use_templates:
-        feature_names += cfg.common.template_features
-
-    if cfg[mode].supervised:
+    if cfg["train"].supervised:
         feature_names += cfg.supervised.supervised_features
 
     return cfg, feature_names
 
-
 def np_example_to_features(
     np_example: FeatureDict,
-    config: ml_collections.ConfigDict,
-    mode: str,
-    is_multimer: bool = False
+    config: ml_collections.ConfigDict
 ):
     np_example = dict(np_example)
 
     seq_length = np_example["seq_length"]
     num_res = int(seq_length[0]) if seq_length.ndim != 0 else int(seq_length)
-    cfg, feature_names = make_data_config(config, mode=mode, num_res=num_res)
+    cfg, feature_names = make_data_config(config, num_res=num_res)
  
     if "deletion_matrix_int" in np_example:
         np_example["deletion_matrix"] = np_example.pop(
@@ -98,56 +75,32 @@ def np_example_to_features(
     )
 
     with torch.no_grad():
-        # if is_multimer:
-        #     features = input_pipeline_multimer.process_tensors_from_config(
-        #         tensor_dict,
-        #         cfg.common,
-        #         cfg[mode],
-        #     )
-        # else:
         features = input_pipeline.process_tensors_from_config(
             tensor_dict,
             cfg.common,
-            cfg[mode],
+            cfg["train"],
         )
 
-    if mode == "train":
-        p = torch.rand(1).item()
-        use_clamped_fape_value = float(p < cfg.supervised.clamp_prob)
-        features["use_clamped_fape"] = torch.full(
-            size=[cfg.common.max_recycling_iters + 1],
-            fill_value=use_clamped_fape_value,
-            dtype=torch.float32,
-        )
-    else:
-        features["use_clamped_fape"] = torch.full(
-            size=[cfg.common.max_recycling_iters + 1],
-            fill_value=0.0,
-            dtype=torch.float32,
-        )
+    p = torch.rand(1).item()
+    use_clamped_fape_value = float(p < cfg.supervised.clamp_prob)
+    features["use_clamped_fape"] = torch.full(
+        size=[cfg.common.max_recycling_iters + 1],
+        fill_value=use_clamped_fape_value,
+        dtype=torch.float32,
+    )
 
     return {k: v for k, v in features.items()}
 
 
 class FeaturePipeline:
-    def __init__(
-        self,
-        config: ml_collections.ConfigDict,
-    ):
+    def __init__(self, config: ml_collections.ConfigDict):
         self.config = config
 
     def process_features(
         self,
-        raw_features: FeatureDict,
-        mode: str = "train",
-        is_multimer: bool = False,
-    ) -> FeatureDict:
-        # if(is_multimer and mode != "predict"):
-        #     raise ValueError("Multimer mode is not currently trainable")
-        
+        raw_features: FeatureDict
+    ) -> FeatureDict:        
         return np_example_to_features(
             np_example=raw_features,
-            config=self.config,
-            mode=mode,
-            is_multimer=is_multimer,
+            config=self.config
         )
